a69528c4c5357b70acdea4e64a12f0adb41a5eb7
Mwenda Claire <mwendaclaire0@gmail.com>
2022-07-16 07:04:10 -0700

    debugiing

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,26 @@
 #ifndef MAIN_H
+
+
+
 #define MAIN_H
+
+
+
+
+
 #include <stdio.h>
 
 
+
+
+
 int largest_number(int a, int b, int c);
+
+
+
+
+
+
 
 void positive_or_negative(int i);
 
@@ -19,4 +36,4 @@ int convert_day(int month, int day);
 
 
 
-#endif
+#endif /*DAN */

1788427bfd1afeb3590373054a6681cc4aac6ad6
Mwenda Claire <mwendaclaire0@gmail.com>
2022-07-16 06:29:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void panctuate(int n)
 {
 
 		int panct;
-			_putchar(',');
-				for (panct = 0; panct < n; panct++)
-						{
-								_putchar(' ');
-										}
 
+		_putchar(',');
+		for (panct = 0; panct < n; panct++)
+		{
+			_putchar(' ');
+		}
 }
 
 /**
@@ -27,67 +27,28 @@ void panctuate(int n)
 void print_numbers(int k, int mod)
 
 {
-
-		if (k < 10)
-				{
-							_putchar(k + '0');
-									if (mod > 0)
-													panctuate(mod);
-
-								}
-
-			else if (k < 100)
-
-					{
-								_putchar((k / 10) + '0');
-
-										_putchar((k % 10) + '0');
-												if (mod > 0)
-																panctuate(mod);
-
-													}
-
-
-
-				else
-
-
-
-						{
-
-
-
-									_putchar((k / 100) + '0');
-
-
-
-											_putchar(((k % 100) / 10) + '0');
-
-
-
-													_putchar((k % 10) + '0');
-
-
-
-															if (mod > 0)
-
-
-
-																			panctuate(mod);
-
-
-
-																}
-
-
-
+	if (k < 10)
+	{
+		_putchar(k + '0');
+		if (mod > 0)
+			panctuate(mod);
+	}
+	else if (k < 100)
+	{
+		_putchar((k / 10) + '0');
+		_putchar((k % 10) + '0');
+		if (mod > 0)
+			panctuate(mod);
+	}
+	else
+	{
+		_putchar((k / 100) + '0');
+		_putchar(((k % 100) / 10) + '0');
+		_putchar((k % 10) + '0');
+		if (mod > 0)
+			panctuate(mod);
+	}
 }
-
-
-
-
-
-
 
 /**
  * print_times_table - print the n x i table
@@ -96,67 +57,26 @@ void print_numbers(int k, int mod)
 
 void print_times_table(int n)
 {
+	int left, right, product, mod, postprod;
 
-		int left, right, product, mod, postprod;
-
-			if (n > 15 || n < 0)
-
-						return;
-				for (left = 0; left <= n; left++)
-						{
-									for (right = 0; right <= n; right++)
-
-										{
-
-																product = left * right;
-																			postprod = (left * (right + 1));
-
-																						if (postprod >= 100)
-
-
-
-																											mod = 1;
-
-
-
-																									else if (postprod >= 10)
-
-
-
-																														mod = 2;
-
-
-
-																												else
-
-
-
-																																	mod = 3;
-
-
-
-																															if (right == n)
-
-
-
-																																				mod = -1;
-
-
-
-																																		print_numbers(product, mod);
-
-
-
-																																				}
-
-
-
-											_putchar('\n');
-
-
-
-												}
-
-
-
+	if (n > 15 || n < 0)
+		return;
+	for (left = 0; left <= n; left++)
+	{
+		for (right = 0; right <= n; right++)
+		{
+			product = left * right;
+			postprod = (left * (right + 1));
+			if (postprod >= 100)
+				mod = 1;
+			else if (postprod >= 10)
+				mod = 2;
+			else
+				mod = 3;
+			if (right == n)
+				mod = -1;
+			print_numbers(product, mod);
+		}
+		_putchar('\n');
+	}
 }

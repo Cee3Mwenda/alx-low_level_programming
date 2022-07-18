56ae4d95a08c40978f1235d4258921b3990ecb3a
Mwenda Claire <mwendaclaire0@gmail.com>
2022-07-18 06:25:52 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,4 @@
-nclude "main.h"
-
-
+include "main.h"
 
 /**
  * _strspn - main description
@@ -11,71 +9,37 @@ nclude "main.h"
 
 unsigned int _strspn(char *s, char *accept)
 
-
-
 {
-
-
 
 int i, j;
 
-
-
 unsigned int contador = 0;
-
-
 
 for (i = 0; s[i] != '\0' ; i++)
 
-
-
 {
-
-
 
 for (j = 0; accept[j] != '\0' ; j++)
 
-
-
 {
-
-
 
 if (s[i] == accept[j])
 
-
-
 {
-
-
 
 contador++;
 
-
-
 break;
 
-
-
 }
 
-
-
 }
-
-
 
 if (s[i] != accept[j])
 
-
-
 break;
 
-
-
 }
-
-
 
 return (contador);
 
